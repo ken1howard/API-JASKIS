97d41b9076122b0a6209d39fbeeb9e053fe4c0ca
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-05-02 17:47:02 -0700

    answered most questions

--- commands.js
+++ commands.js
@@ -4,26 +4,44 @@
 // GETTING STARTED
 // 1. Create a database called jaskis
 
+usejaskis
 
 // 2. Create a collection called bounties
 
+db.createCollection('bounties')
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
 
+db.bounties.insertOne({ name: "Thanoceros"})
 
 // 2. Query for all bounties in the bounties collection
 
+db.bounties.find()
 
 // 3. Insert many bounties at once using the given objects
+
+db.bounties.insertOne({ species: "Rhinoceros", 
+location: "Grassland", 
+wantedFor: "Eating too much grass", 
+client: "Songbird", 
+reward: 10000, 
+captured: false})
+
 
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
 
+db.bounties.find({ location: "Grassland"})
+
 // 2. Query for all bounties with a reward worth 10000 or more
 
+db.bounties.find({ reward: { $gt: 10000 } })
+
 // 3. Query for all bounties, but exclude the client attribute from being shown
+
+db.bounties.aggregate([{$project:{"client":0}}])
 
 // 4. Query for a Groundhog in the Woodlands
 
